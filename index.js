f008c8a2d35ba19efca2605695db1d8c53b4dc24
Hiren Faldu <hiren.f@crestinfosystems.net>
2020-02-18 10:50:16 +0530

    subscription added with graphQL

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server');
+const { ApolloServer, UserInputError, gql, PubSub } = require('apollo-server');
 const con = require('./db');
 var bcrypt = require('bcryptjs');
+const pubsub = new PubSub();
 // The GraphQL schema
 const typeDefs = gql`
   type Query {
     users: [User]
   }
+
   type Mutation {
-      createUser(name: String!, email: String!, title: String!, password: String) :  User,
+      createUser(name: String!, email: String!, title: String!, password: String, id: ID) :  User
       me (email: String!, password: String!) : User
+      deleteUser(id: ID!) : String
   }
+
+  type Subscription {
+    userAdded: User!,
+    userDeleted: ID!
+  }
+
   type User {
-    email: String,
-    name: String,
-    id: ID,
-    password: String,
-    title: String,
+    email: String
+    name: String
+    id: ID
+    password: String
+    title: String
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
+  Subscription: {
+    userAdded: {
+      subscribe: () => {
+        return pubsub.asyncIterator(['USER_ADDED'])
+      },
+    },
+    userDeleted: {
+      subscribe: () => {
+        return pubsub.asyncIterator(['USER_DELETED'])
+      },
+    }
+  },
   Query: {
-    users: () => {
-        return getUsers();
+    users: (parent, args, context, info) => {
+      const searched_type = info.fieldNodes.find((field_node) => field_node.name.value == 'users')
+      let selected_fields = ['*']
+      if(searched_type) {
+        selected_fields = searched_type.selectionSet.selections.map((selection) => selection.name.value)
+      }
+        return getUsers(selected_fields);
     }
   },
   Mutation: {
@@ -32,13 +58,29 @@ const resolvers = {
             if(found_data) {
                 return found_data;
             }
-            console.log('throw')
-            throw new UserInputError('Incorrect credentials!');
+            throw new UserInputError('Incorrect credentials!', 'email');
             
         },
-      createUser: (parent, user) => {
-        user.password = bcrypt.hashSync(user.password)
-        return createUser(user);
+      createUser: async (parent, user) => {
+        if(user.password) {
+          user.password = bcrypt.hashSync(user.password)
+        } else {
+          delete user.password
+        }
+        if(user.id) {
+          const id = user.id
+          delete user.id
+          const updated_user = await updateUser(user, id);
+          pubsub.publish('USER_ADDED', { userAdded: updated_user });
+          return updated_user
+        }
+        const created_user = await createUser(user);
+        pubsub.publish('USER_ADDED', { userAdded: created_user });
+        return created_user
+      },
+      deleteUser: (parent, {id}) => {
+        pubsub.publish('USER_DELETED', { userDeleted: id });
+        return deleteUser(id);
       }
   }
 };
@@ -48,8 +90,8 @@ const server = new ApolloServer({
   resolvers,
   
 });
-server.listen(4000).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+server.listen(4000).then(({ url, subscriptionsUrl }) => {
+  console.log(`Server ready at ${url} and subscription at ${subscriptionsUrl }`);
 });
 
 function getUser(email, password) {
@@ -64,9 +106,9 @@ function getUser(email, password) {
         });
     });
 }
-function getUsers() {
+function getUsers(selected_fields) {
     return new Promise((resolve, reject) => {
-      con.query("SELECT * FROM users;", (err, result) => {
+      con.query(`SELECT ${selected_fields.join(',')} FROM users;`, (err, result) => {
           if (err) reject(err);
           resolve(result);
       });
@@ -82,4 +124,24 @@ function createUser(user_data) {
             });
         });
     });
+}
+
+function updateUser(user_data, id) {
+    return new Promise((resolve, reject) => {
+        con.query(`UPDATE users SET ? WHERE id= ?;`, [user_data, id], (err, result) => {
+            if (err) return reject(err);
+            con.query("SELECT * FROM users WHERE ?;", {id: id}, (err, result) => {
+                resolve(result[0])
+            });
+        });
+    });
+}
+
+function deleteUser(id) {
+  return new Promise((resolve, reject) => {
+      con.query(`DELETE FROM users WHERE id = ?;`, id, (err, result) => {
+          if (err) return reject(err);
+          resolve('User deleted Successfully!');
+      });
+  });
 }
